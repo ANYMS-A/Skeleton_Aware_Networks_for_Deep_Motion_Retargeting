e569a550d7c9de5be84cb3e2f2898d12f50014d1
allancodeman <allancodeman@163.com>
2020-07-15 10:19:57 +0800

    remove virtual joints when write bvh files

--- datasets/bvh_writer.py
+++ datasets/bvh_writer.py
@@ -1,11 +1,10 @@
 import numpy as np
 from utils.Quaternions import Quaternions
-from utils import build_joint_topology
 from utils.Filtering import gaussian_smooth
 
 
 # rotation with shape frame * J * 3
-def write_bvh(parent, offset, rotation, position, names, frametime, order, path, endsite=None):
+def write_bvh(parent, offset, rotation, position, names, frametime, order, path):
     file = open(path, 'w')
     frame = rotation.shape[0]
     joint_num = rotation.shape[1]
@@ -14,7 +13,7 @@ def write_bvh(parent, offset, rotation, position, names, frametime, order, path,
     file_string = 'HIERARCHY\n'
 
     def write_static(idx, prefix):
-        nonlocal parent, offset, rotation, names, order, endsite, file_string
+        nonlocal parent, offset, rotation, names, order, file_string
         if idx == 0:
             name_label = 'ROOT ' + names[idx]
             channel_label = 'CHANNELS 6 Xposition Yposition Zposition {}rotation {}rotation {}rotation'.format(*order)
@@ -63,11 +62,43 @@ class BvhWriter(object):
         :param offset: a torch.Tensor with shape [3, simplified_num_joints]
         """
         offset = offset.permute(1, 0).cpu().detach().numpy()
-        self.parent, self.offset, self.names, self.edge2joint = build_joint_topology(edges, names, offset)
-        self.joint_num = len(self.parent)
+        self.edges = edges
+        self.names = names
+        self.offset = offset
+        self.joint_num = len(self.names)
 
-    # position, rotation with shape T * J * (3/4)
-    def write(self, rotations, positions, order, path, frametime=1.0/30, offset=None, root_y=None):
+    def edge_rotation_to_joint_rotation(self, rotation):
+        """
+        Since the rotation of edges are parents' joint rotation, we need to convert them into the bvh file format
+        :param rotation: numpy array with shape [frame, J-1, 3], use euler angle not quaternion
+        :return: numpy array with shape [frame, J, 3]
+        """
+        f, j_mone, _ = rotation.shape
+        children_of_joint = [list() for each in range(self.joint_num)]
+        parent_of_joint = [-1] * self.joint_num
+        for edge in self.edges:
+            parent_idx = edge[0]
+            child_idx = edge[1]
+            children_of_joint[parent_idx].append(child_idx)
+            parent_of_joint[child_idx] = parent_idx
+        # set root's parent be itself
+        parent_of_joint[0] = 0
+
+        rotation_new = np.zeros([f, j_mone+1, 3])
+        for idx in range(self.joint_num):
+            tmp_children = children_of_joint[idx]
+            tmp_children = [each - 1 for each in tmp_children]
+            if len(tmp_children) == 0:
+                continue
+            else:
+                tmp_rotations = rotation[:, tmp_children, :]
+                tmp_rotations = tmp_rotations.mean(axis=1)
+                rotation_new[:, idx, :] = tmp_rotations
+        return rotation_new, parent_of_joint
+
+    def write(self, rotations, positions, order, path, frametime=1.0/30):
+        # position in shape [frame, 3]
+        # rotation in shape [frame, J-1, 4]
         if order == 'quaternion':
             norm = rotations[:, :, 0] ** 2 + rotations[:, :, 1] ** 2 + rotations[:, :, 2] ** 2 + rotations[:, :, 3] ** 2
             norm = np.repeat(norm[:, :, np.newaxis], 4, axis=2)
@@ -75,17 +106,11 @@ class BvhWriter(object):
             rotations = Quaternions(rotations)
             rotations = np.degrees(rotations.euler())
             order = 'xyz'
+        # convert the rotation, [frame, J, 3]
+        rotations, parent_of_joint = self.edge_rotation_to_joint_rotation(rotations)
+        return write_bvh(parent_of_joint, self.offset, rotations, positions, self.names, frametime, order, path)
 
-        rotations_full = np.zeros((rotations.shape[0], self.joint_num, 3))
-        for idx, edge in enumerate(self.edge2joint):
-            if edge != -1:
-                rotations_full[:, idx, :] = rotations[:, edge, :]
-        if root_y is not None: rotations_full[0, 0, 1] = root_y
-
-        if offset is None: offset = self.offset
-        return write_bvh(self.parent, offset, rotations_full, positions, self.names, frametime, order, path)
-
-    def write_raw(self, motion, order, path, frametime=1.0/30, root_y=None):
+    def write_raw(self, motion, order, path, frametime=1.0/30):
         """
         :param motion: A tensor with shape [(simple_joint_num - 1) * 4 + 3, frame]
         :return:
@@ -97,8 +122,9 @@ class BvhWriter(object):
         # smooth the position and rotation info
         rotations, positions = gaussian_smooth(rot=rotations, pos=positions)
         if order == 'quaternion':
+            # [frame, J-1, 4]
             rotations = rotations.reshape((motion.shape[0], -1, 4))
         else:
             rotations = rotations.reshape((motion.shape[0], -1, 3))
-
-        return self.write(rotations, positions, order, path, frametime, root_y=root_y)
+        # position in shape [frame, 3]
+        return self.write(rotations, positions, order, path, frametime)
